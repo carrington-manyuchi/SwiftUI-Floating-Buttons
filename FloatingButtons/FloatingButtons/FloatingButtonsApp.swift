cd1aebafd298b685f1028c311a1a7f3dfe7c498b
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-17 20:55:16 +0200

    Button

---
+++ FloatingButtons/FloatingButtons/FloatingButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButtonsApp.swift
+//  FloatingButtons
+//
+//  Created by Manyuchi, Carrington C on 2024/08/17.
+//
+
+import SwiftUI
+
+@main
+struct FloatingButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
